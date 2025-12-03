b733b57a7c4233e9444182f627aaf859f7ac39db
barbless1 <rataumaile@gmail.com>
2025-12-03 23:00:39 +0100

    Added Animals model/Login page in accueil.html

--- zmanager/zmanager_app/models.py
+++ zmanager/zmanager_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):                     #Le premier modèle défini est celui de l'animal. Il fait référence au modèle de la bdd "mysql_template.sql"
+    name = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
+    age = models.IntegerField()
+    arrival_date = models.DateField()
+
+    def __str__(self):
+        return self.name
