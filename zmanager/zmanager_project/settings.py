e1514a634964628ce33ef6be35e988dc028e5998
barbless1 <rataumaile@gmail.com>
2025-12-08 19:01:21 +0100

    Fixed first_name usage for profile pictures, Changed Locale to french to display dates better, adjusted animal_edit.html style

--- zmanager/zmanager_project/settings.py
+++ zmanager/zmanager_project/settings.py
@@ -111,11 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr' #You need to change this line as it's only for your local, we needed to display dates in french so we adjusted this one
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
